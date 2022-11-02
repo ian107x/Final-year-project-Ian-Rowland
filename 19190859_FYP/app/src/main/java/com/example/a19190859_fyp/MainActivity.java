9fa4eb8e80ae8f4edfad0191b97a574e80d009bb
ian107x <imjrowland@gmail.com>
2022-11-02 12:52:46 +0000

    initial changes
    create xml for view data activity
    
    update functionality for main activity

--- 19190859_FYP/app/src/main/java/com/example/a19190859_fyp/MainActivity.java
+++ 19190859_FYP/app/src/main/java/com/example/a19190859_fyp/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.a19190859_fyp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startGameButton;
+    Button startViewDataButton;
+    TextView introductionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startGameButton = (Button) findViewById(R.id.startgamebutton);
+        startViewDataButton = (Button) findViewById(R.id.viewdatabutton);
+        introductionText = (TextView) findViewById(R.id.introductiontext);
+
+        startGameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent startGameIntent = new Intent(getBaseContext(), gamePlayActivity.class);
+                startActivity(startGameIntent);
+            }
+        });
+
+        startViewDataButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent viewDataIntent = new Intent(getBaseContext(), viewDataActivity.class);
+                startActivity(viewDataIntent);
+            }
+        });
     }
 }
